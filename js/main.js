5cebbc969b6f921c90f403eb9bb724f0ac2ed518
DIEGO FERNANDES MENDES <diego.mendes.piano@gmail.com>
2021-01-21 22:53:24 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
 
-var nome = "Diego Mendes";
-var n1 = 36;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo";
+
+
+// var d = new Date();
+// alert(d)
+// alert(d.getUTCFullYear())
+
+
+// var count;
+// for (count = 0; count < 5; count++) {
+//     console.log(count)    
+// };
+
+// var count = 0;
+// while (count <= 5) {
+//     console.log(count);
+//     count = count + 1;
+// }
+
+// var idade = prompt("Qual a sua idade?")
+// if (idade >= 18) {
+//     alert("Maior de idade");
+// } else {
+//     alert("Menor de idade");
+// };
+
+// var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+// console.log(frutas);
+// alert(frutas[1].nome)
+
+// var fruta = {nome: "maça", cor: "vermelha"};
+// console.log(fruta.nome);
+// alert(fruta.cor)
+
+
+// var lista = ["maçã", "pera", "uva"];
+
+// lista.push("laranja");
+// lista.pop()
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(' - '))
+// alert(lista[0]);
+
+
+// var nome = "Diego Mendes";
+// var n1 = 36;
+// var n2 = 10;
+// var frase = "Japão é o melhor time do mundo";
 // alert(nome + " tem " + idade + " anos" );
 // alert(idade + idade2)
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toUpperCase())
+// console.log(nome);
+// console.log(n1 * n2);
+// console.log(frase.toUpperCase())
 // console.log(frase.replace("Japão", "Brasil"));
 //alert(frase.replace("Japão", "Brasil"));
 
