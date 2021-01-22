c1537a535fc954f7a2381eabd84eedbe945c33eb
DIEGO FERNANDES MENDES <diego.mendes.piano@gmail.com>
2021-01-21 21:50:08 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+var nome = "Diego Mendes";
+var n1 = 36;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + idade + " anos" );
+// alert(idade + idade2)
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase())
+// console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+
+
